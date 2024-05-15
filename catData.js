c31f95ba0fa1bf4d982fe650cf8d25af8843087b
Joshua Federman <97866996+JoshTheConfused@users.noreply.github.com>
2024-05-15 18:40:16 +0000

    All assigned work done

--- catData.js
+++ catData.js
@@ -4,24 +4,24 @@ let button = document.querySelector("button")
 button.addEventListener('click', function() {
 
 
-// 1. Create a fetch request accessing the db.json file
-
-
+    // 1. Create a fetch request accessing the db.json file
+    fetch("db.json")
     
-// 2. Use the .then method and convert the response to JSON
-
-
-
-// 3. Use a second .then method to access the data property of the JSON.
-  
-// In the body of the second .then method:
+    // 2. Use the .then method and convert the response to JSON
+        .then(response => response.json())
+    
+    // 3. Use a second .then method to access the data property of the JSON.
+        .then(data => {
+            
+    // In the body of the second .then method:
     // 4. Using dot notation, store the facts of the data in a variable called 'catFacts'.
+        let catFacts = data.facts;
+            
     // 5. Uncomment the randomFact variable.
-  
-    // let randomFact = catFacts[Math.floor(Math.random() * catFacts.length)].detail;
+        let randomFact = catFacts[Math.floor(Math.random() * catFacts.length)].detail;
 
     // 6. Select the para variable and update the innerHTML property to the randomFact variable.
-
-      
+        para.innerHTML = randomFact;
+        });
 
 });
--- script.js
+++ script.js
@@ -6,28 +6,17 @@ search.addEventListener("click", function() {
 
   let pokemon = document.querySelector(".pokemon").value;
   let pokemonURL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-
-
   
-// 1. Create a fetch request using the pokemonURL
-
-
-// 2. Use the .then method and convert the response to JSON
-
-  
-// 3. Use the .then method to access the data property of the JSON.
-
-  
-// 4. Log the data to the console.
-  
-
-
-  
+  // 1. Create a fetch request using the pokemonURL
+  fetch(pokemonURL)
+  // 2. Use the .then method and convert the response to JSON
+    .then(response => response.json())
+  // 3. Use the .then method to access the data property of the JSON.
+    .then(data => {
+  // 4. Log the data to the console.
+    console.log(data);
+    pokemonName.innerHTML = `<h2>${data.name}</h2>`;
+    pokemonImage.innerHTML = `<img src="${data.sprites.front_default}">`;
+    });
   
 });
-
-
-
-// 5. Uncomment and replace the logged data with the two lines below:
-// pokemonName.innerHTML = `<h2>${data.name}</h2>`;
-// pokemonImage.innerHTML = `<img src="${data.sprites.front_default}">`;
